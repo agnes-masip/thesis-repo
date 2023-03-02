dc3572e62ce538a926227b9ee2c936383d5772b9
Michelle <xmichellegs@gmail.com>
2023-03-02 14:58:48 +0100

    theme added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,32 @@
-// import logo from './logo.svg';
-import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/views/home';
 import List from './components/views/list';
-  
+import './App.css';
+
+
+const theme = createTheme({
+  palette: {
+    primary: '#FF74B1',
+    secondary: {
+      main: '#FFA1CF',
+    },
+    dark: {
+      main: '#FFD6EC'
+    }
+  }
+});
 
 function App() {
   return (
     <div className="app">
+      <ThemeProvider theme={theme}>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='home' element={<Home/>} />
           <Route path='list' element={<List/>} />
         </Routes>
+      </ThemeProvider>
     </div>
   );
 }
