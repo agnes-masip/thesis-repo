b5837cc1c490b884fc00101e36d1bf8789ff3dd8
Najma Christen <najma@gmx.ch>
2023-03-02 17:04:06 +0100

    some components added

--- my-app/src/components/views/home.js
+++ my-app/src/components/views/home.js
@@ -1,14 +1,39 @@
-
+import React from 'react';
 import '../../App.css';
+import {Box, Button, Card, CardContent, Typography} from '@mui/material';
+import {List, ListItem, ListItemText} from "@mui/material";
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          yeehaw home
-        </p>
-      </header>
+    <div className="App" >
+        <div className="page-title" color="primary">
+            <Typography variant="h3" color="primary">
+                Home
+            </Typography>
+        </div>
+        <Box color="secondary">
+            <Card >
+                <CardContent >
+                    User
+                </CardContent>
+            </Card>
+        </Box>
+        <Box>
+            <Typography variant="h3" color="primary">
+                Lists
+            </Typography>
+            <List>
+                <ListItem>
+                    <ListItemText>
+                        MyFirstList
+                    </ListItemText>
+                </ListItem>
+            </List>
+
+        </Box>
+        <Button>
+            Create list
+        </Button>
     </div>
   );
 }
