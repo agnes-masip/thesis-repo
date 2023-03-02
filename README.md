074827108db9d8d3677ab97428e3be42fcf8b065
unknown <agnesmgomez@gmail.com>
2023-03-02 16:49:34 +0100

    deployment and database done

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ### `npm start`
 
 `npm install -g @aws-amplify/cli`
+
+Run `amplify pull` to sync future upstream changes
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "thesisrepo": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
