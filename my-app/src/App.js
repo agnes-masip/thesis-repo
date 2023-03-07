f9fd6548fbaaaa6e5bacd55fe3866a6eca52c50c
Najma Christen <najma@gmx.ch>
2023-03-07 18:35:27 +0100

    color and grid

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import { createTheme, colors, ThemeProvider } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/views/home';
 import List from './components/views/list';
+import NavBar from './components/navbar';
 import './App.css';
 
 
@@ -17,7 +18,7 @@ const theme = createTheme({
       main: '#FFD6EC'
     },
     background: {
-      paper: '#FFA1CF',
+      main: '#fde4f2'
     }
   },
   typography: {
@@ -37,6 +38,7 @@ function App() {
   return (
     <div className="app">
       <ThemeProvider theme={theme}>
+        <NavBar/>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='home' element={<Home/>} />
--- my-app/src/components/views/home.js
+++ my-app/src/components/views/home.js
@@ -1,59 +1,161 @@
 import React from 'react';
 import '../../App.css';
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {Box, Button, Card,Grid, CardContent, Typography} from '@mui/material';
-import {List, ListItem, ListItemText, ListItemButton, ListItemIcon} from "@mui/material";
+import {Box, Button, Card, Grid, CardContent, Typography} from '@mui/material';
+import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
 
 
-function Home() {
 
-    let navigate = useNavigate();
-    const goToList = () => {
+const initialRows = [
+    {
+        id: 1,
+        listname: 'Important Papers',
+        creator: 'Schrijnemaekers',
+    },
+    {
+        id: 2,
+        listname: 'Some other irrelevant Papers',
+        creator: 'Masip-Gomez',
+    },
+    {
+        id: 3,
+        listname: 'Another list of papers',
+        creator: 'van Brummelen',
+    },
+    {
+        id: 4,
+        listname: 'My Papers',
+        creator: 'Christen',
+    },
+];
+
+
+const initialUser = {"name": "najma", "mail": "some@mail.ch"}
+
+
+
+
+export default function Home() {
+    const [user, setUser] = React.useState(initialUser);
+    const [rows, setRows] = React.useState(initialRows);
+
+    // Currently only deletes item from list visually
+    const deleteSource = React.useCallback(
+        (id) => () => {
+            setTimeout(() => {
+                setRows((prevRows) => prevRows.filter((row) => row.id !== id));
+            });
+        },
+        [],
+    );
+
+    // Currently does nothing
+    const goToList = React.useCallback(
+        (id) => () => {
+        },
+        [],
+    );
+
+    // Currently does nothing
+    const createList = React.useCallback(
+        (id) => () => {
+        },
+        [],
+    );
+
+    //goes to list view
+/*    let navigate = useNavigate();
+    const goToList = (id) => {
         let path = 'list';
         navigate(path);
     }
+*/
+
+/*
+    const columns = React.useMemo(
+        () => [
+            { field: 'listname', headerName: 'Listname', headerClassName: 'data-grid-header', type: 'string', flex: 1.5 },
+            { field: 'creator', headerName: 'Creator', headerClassName: 'data-grid-header', type: 'string', flex: 1 },
+            {
+                field: 'actions',
+                headerClassName: 'data-grid-header',
+                type: 'actions',
+                flex: 0.25,
+                getActions: (params) => [
+                    <GridActionsCellItem
+                        icon={<DeleteIcon />}
+                        label="Delete"
+                        onClick={deleteSource(params.id)}
+                    />,
+                    <GridActionsCellItem
+                        icon={<EditIcon />}
+                        label="Edit"
+                        onClick={goToList(params.id)}
+                    />,
+
+                ],
+            },
+        ],
+        [deleteSource, goToList],
+    );
+*/
+    const columns = [
+        { field: 'id', headerName: 'ID', headerClassName: 'data-grid-header', type: 'number', flex: 1.5 },
+        { field: 'listname', headerName: 'Listname', headerClassName: 'data-grid-header', type: 'string', flex: 1.5 },
+        { field: 'creator', headerName: 'Creator', headerClassName: 'data-grid-header', type: 'string', flex: 1 },
+    ];
+
+    //<DataGrid columns={columns} rows={rows}/>
+
   return (
-    <div className="App" style={{ margin: '10px'}}>
-        <div className="page-title" >
-            <Typography variant="h3" color="primary">
+    <div className="Content" >
+        <div className="Title" >
+            <Typography variant="h4" align="left" color="primary">
                 Home
             </Typography>
         </div>
-        <Box style={{ marginTop: '50px', marginBottom: '50px', marginLeft: '10%', marginRight: '10%'}}>
-            <Card >
-                <CardContent >
-                    <Typography variant="h4" align="left">
-                      User
-                    </Typography>
-                </CardContent>
-            </Card>
-        </Box>
+        <div>
+            <Box style={{ marginTop: '50px', marginBottom: '50px', marginLeft: '10%', marginRight: '10%'}}>
+                <Typography variant="h6" align="left" color="primary">
+                    User
+                </Typography>
+                <Card >
+                    <CardContent >
+                        <Typography variant="h7" align="left" color="primary"> Username : </Typography>
+                        <Typography variant="h7" align="left" > {user.name} </Typography>
+                        <br/>
+                        <Typography variant="h7" align="left" color="primary"> Mail : </Typography>
+                        <Typography variant="h7" align="left" > {user.mail}</Typography>
+                    </CardContent>
+                </Card>
+            </Box>
 
-        <Box style={{ marginTop: '50px', marginBottom: '50px', marginLeft: '10%', marginRight: '10%'}}>
-            <Grid container spacing={2}>
-                <Grid item xs={6} md={8}>
-                    <Typography variant="h2" align="left" color="primary">
-                        Lists
-                    </Typography>
-                    <List>
-                        <ListItem button="true" onClick={() => goToList()}>
-                            <ListItemText>
-                                My First List
-                            </ListItemText>
-                        </ListItem>
-                    </List>
-                </Grid>
-                <Grid item xs={6} md={4}>
-                    <Button variant="contained" color="primary" onClick={() => goToList()}>
-                        <Typography variant>
-                            Create list
+            <Box style={{ marginTop: '50px', marginBottom: '50px', marginLeft: '10%', marginRight: '10%'}}>
+                <Grid container spacing={2}>
+                    <Grid item xs={6} md={8}>
+                        <Typography variant="h6" align="left" color="primary">
+                            My Lists
                         </Typography>
-                    </Button>
+                        <Card sx={{height: 800, width: '100%' }}>
+
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6} md={4}>
+                        <Button variant="contained" color="primary" endIcon={<AddIcon />} onClick={() => createList()}>
+                            <Typography variant>
+                                Create list
+                            </Typography>
+                        </Button>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Box>
+            </Box>
+        </div>
     </div>
   );
 }
 
-export default Home;
+
