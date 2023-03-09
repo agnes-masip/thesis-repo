3301e4b74a17a2623fea19533dddf9bd289e0b8b
renskelotte48 <49675916+renskelotte48@users.noreply.github.com>
2023-03-09 13:55:20 +0100

    first cleanup + main merge

--- src/components/api/lists.js
+++ src/components/api/lists.js
@@ -1,7 +1,7 @@
 import { API } from "aws-amplify";
-import { createList, updateList, deleteList} from '../graphql/mutations';
+import { createList, updateList, deleteList} from '../../graphql/mutations';
 
-import { listLists, getList } from "../graphql/queries";
+import { listLists, getList } from "../../graphql/queries";
 
 const createNewList = async () =>{
     const newList = await API.graphql({
@@ -63,5 +63,53 @@ const oneList = await API.graphql({
 });    
 }
 
+export const getListById = async(listID) => {
+    const listData = await API.graphql({
+      query: getList,
+      variables: { id: listID }
+    });
+    return listData.data.getList;
+}
 
+// alter papers in list
+export const deletePaperFromList = async(listId, paperId) => {
+    try{
+      const list = await getListById(listId);
+      let listData = list;
+      let listPapers = listData.papers;
+      if (listPapers.includes(paperId)) { listPapers = listPapers.filter(id => id != paperId); }
+      listData.papers = listPapers;
+      await updateListById(listData);
+    } catch (error) {
+      console.error("Error on deleting paper from list", error);
+    }
+}
+
+export const addPaperToList = async(listId, paperId) => {
+    try {
+      const list = await getListById (listId);
+      let listData = list;
+      let listPapers = listData.papers;
+      if (!listPapers.includes(paperId)) { listPapers.push(paperId); }
+      listData.papers = listPapers;
+      await updateListById(listData);
+    } catch (error) {
+      console.error('Error on adding paper to list', error);
+    }
+}
+
+export const updateListById = async(listData) => {
+    try {
+      delete listData["createdAt"];
+      delete listData["updatedAt"];
+      await API.graphql({
+        query: updateList,
+        variables: {
+            input: listData
+        }
+      });
+    } catch (error) {
+      console.error("Error on update list", error);
+    }
+}
 
--- src/components/api/papers.js
+++ src/components/api/papers.js
@@ -1,68 +1,81 @@
 import { API } from "aws-amplify";
-import { createPaper, updatePaper, deletePaper } from '../graphql/mutations';
+import { createPaper, updatePaper, deletePaper } from '../../graphql/mutations';
 
-import { listPapers, getPaper } from "../graphql/queries";
+import { listPapers, getPaper } from "../../graphql/queries";
 
-const newPaper = await API.graphql({
-    query: createPaper,
-    variables: {
-        input: {
-		"title": "Lorem ipsum dolor sit amet",
-		"description": "Lorem ipsum dolor sit amet",
-		"likes": 1020,
-		"author": [],
-		"journal": "Lorem ipsum dolor sit amet",
-		"year": 1020,
-		"volume": "Lorem ipsum dolor sit amet",
-		"issue": "Lorem ipsum dolor sit amet",
-		"doi": "Lorem ipsum dolor sit amet",
-		"issn": "Lorem ipsum dolor sit amet",
-		"citationStorageLocation":  "https://www.google.com/"
-	}
+export const newPaper = async (paperData) => {
+	await API.graphql({
+		query: createPaper,
+		variables: {
+			input: {
+			"title": "Lorem ipsum dolor sit amet",
+			"description": "Lorem ipsum dolor sit amet",
+			"likes": 1020,
+			"author": [],
+			"journal": "Lorem ipsum dolor sit amet",
+			"year": 1020,
+			"volume": "Lorem ipsum dolor sit amet",
+			"issue": "Lorem ipsum dolor sit amet",
+			"doi": "Lorem ipsum dolor sit amet",
+			"issn": "Lorem ipsum dolor sit amet",
+			"citationStorageLocation":  "https://www.google.com/"
+		}
+		}
+	});
+}
+
+export const updatedPaper = async(paperData) => {
+	await API.graphql({
+		query: updatePaper,
+		variables: {
+			input: {
+				"title": paperData["title"],
+				"description": paperData["description"],
+				"likes": paperData["likes"],
+				"author": paperData["author"],
+				"journal": paperData["journal"],
+				"year": paperData["year"],
+				"volume": paperData["volume"],
+				"issue": paperData["issue"],
+				"doi": paperData["doi"],
+				"issn": paperData["issn"],
+				"citationStorageLocation": paperData["citationStorageLocation"]
+			}
+		}
+	})
+}
+
+
+export const deletePaperById = async (id) => {
+    try {
+        await API.graphql({
+          query: deletePaper,
+          variables: {
+              input: {
+                  id: id
+              }
+          }
+      });
+    } catch (error) {
+        console.log('error on deleting paper', error);
     }
-});
-
-const updatedPaper = await API.graphql({
-    query: updatePaper,
-    variables: {
-        input: {
-		"title": "Lorem ipsum dolor sit amet",
-		"description": "Lorem ipsum dolor sit amet",
-		"likes": 1020,
-		"author": [],
-		"journal": "Lorem ipsum dolor sit amet",
-		"year": 1020,
-		"volume": "Lorem ipsum dolor sit amet",
-		"issue": "Lorem ipsum dolor sit amet",
-		"doi": "Lorem ipsum dolor sit amet",
-		"issn": "Lorem ipsum dolor sit amet",
-		"citationStorageLocation":  "https://www.google.com/"
-	}
-    }
-});
-
-
-const deletedPaper = await API.graphql({
-    query: deletePaper,
-    variables: {
-        input: {
-            id: "YOUR_RECORD_ID"
-        }
-    }
-});
+  };
 
 //QUERIES
 
 // List all items
-const allPapers = await API.graphql({
-    query: listPapers
-});
-console.log(allPapers);
+export const getAllPapers = async() => {
+	await API.graphql({
+		query: listPapers
+	});
+}
 
 // Get a specific item
-const onePaper = await API.graphql({
-    query: getPaper,
-    variables: { id: 'YOUR_RECORD_ID' }
-});
+export const getPaperById = async(paperId) => {
+	await API.graphql({
+		query: getPaper,
+		variables: { id: paperId }
+	});
+}
 
 
