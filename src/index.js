2e0afba4c6277ae438731ab26fed150a7bfcb644
renskelotte48 <49675916+renskelotte48@users.noreply.github.com>
2023-03-16 15:28:44 +0100

    react downgrade to 17

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import *  as ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root');
+render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
-
+  </React.StrictMode>,
+  root
 );
