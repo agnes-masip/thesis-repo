da8c5917d10ff48488d64bce106fb90388df7354
renskelotte48 <49675916+renskelotte48@users.noreply.github.com>
2023-03-19 20:07:07 +0100

    move tsconfig.json, add sleep to tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://main.d1ranocpeacs6g.amplifyapp.com/',
+    // baseUrl: 'https://main.d1ranocpeacs6g.amplifyapp.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -1,9 +1,15 @@
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 describe('Sign Up', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
   })
   
-  it('sign up', () => {
+  it('sign up', async() => {
     // https://on.cypress.io/submit
     cy.get('.signUpClass')
       .find('[id="username-signup-input"]').type('cypress')
@@ -13,12 +19,14 @@ describe('Sign Up', () => {
       .find('[id="password-signup-input"]').type('cypressPassword')
     
     cy.get('.signUpButtonClass').click()
+
+    await sleep(3000);
     
     cy.url().should('include','cypress')
 
   })
 
-  it('sign in', () => {
+  it('sign in', async() => {
     // https://on.cypress.io/submit
     cy.get('.signInClass')
       .find('[id="email-signin-input"]').type('canary@gmail.com')
@@ -26,6 +34,8 @@ describe('Sign Up', () => {
       .find('[id="password-signin-input"]').type('canary')
     
     cy.get('.signInButtonClass').click()
+
+    await sleep(3000);
     
     cy.url().should('include','cypress')
 
